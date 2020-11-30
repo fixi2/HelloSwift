5baa74e032b420130478ef149c72e26c5e96f313
JunBeom Jeon <fixi2@naver.com>
2020-12-01 01:11:29 +0900

    Label, Button, AutoLayout

---
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by USER on 2020/12/01.
+//  Copyright © 2020 junb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func SimpleBtn(_ sender: Any) {
+        print("Click Button")
+    }
+}
+
